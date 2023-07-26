d934f65323150ac2a449491e44065ba07d4b821c
Karl Fredrik Haugland <karlfredrik.haugland@oda.com>
2023-07-26 10:48:33 +0200

    👔 improvement: Add more logic for supplier invs

--- oda_wd_client/service/resource_management/types.py
+++ oda_wd_client/service/resource_management/types.py
@@ -24,6 +24,12 @@ class TaxOption(WorkdayReferenceBaseModel):
     workday_id_type: Literal["Tax_Option_ID"] = "Tax_Option_ID"
 
 
+class TaxCode(WorkdayReferenceBaseModel):
+    _class_name = "Tax_CodeObject"
+    workday_id: str
+    workday_id_type: Literal["Tax_Code_ID"] = "Tax_Code_ID"
+
+
 class Supplier(WorkdayReferenceBaseModel):
     _class_name = "SupplierObject"
     workday_id: str
@@ -66,9 +72,44 @@ class Supplier(WorkdayReferenceBaseModel):
     tax_id_us: str | None
 
 
+class TaxRate(WorkdayReferenceBaseModel):
+    _class_name = "Tax_RateObject"
+    workday_id_type: Literal["Tax_Rate_ID"] = "Tax_Rate_ID"
+
+
+class TaxRecoverability(WorkdayReferenceBaseModel):
+    _class_name = "Tax_RecoverabilityObject"
+    workday_id_type: Literal[
+        "Tax_Recoverability_Object_ID"
+    ] = "Tax_Recoverability_Object_ID"
+
+
+class SpendCategory(WorkdayReferenceBaseModel):
+    _class_name = "Spend_CategoryObject"
+    workday_id_type: Literal["Spend_Category_ID"] = "Spend_Category_ID"
+
+
+class TaxRateOptionsData(BaseModel):
+    tax_rate: TaxRate
+    tax_recoverability: TaxRecoverability = TaxRecoverability(
+        workday_id="Fully_Recoverable"
+    )
+    tax_option: TaxOption = TaxOption(workday_id="CALC_TAX_DUE")
+
+
+class CostCenter(WorkdayReferenceBaseModel):
+    _class_name = "Accounting_WorktagObject"
+    workday_id_type: Literal["Cost_Center_Reference_ID"] = "Cost_Center_Reference_ID"
+
+
 class SupplierInvoiceLine(BaseModel):
     order: int | None
     description: str | None
+    tax_rate_options_data: TaxRateOptionsData
+    tax_applicability: TaxApplicability
+    tax_code: TaxCode
+    spend_category: SpendCategory
+    cost_center: CostCenter
     amount: Decimal = Field(max_digits=18, decimal_places=3)
 
 
@@ -81,8 +122,7 @@ class SupplierInvoice(BaseModel):
     due_date: date
     total_amount: Decimal = Field(max_digits=26, decimal_places=6)
     tax_amount: Decimal = Field(max_digits=26, decimal_places=6)
-    # ENTER_TAX_DUE is "Enter tax due to supplier", which is needed to manually set tax amount in invoices
-    tax_option: TaxOption = TaxOption(workday_id="ENTER_TAX_DUE")
+    tax_option: TaxOption
 
     lines: list[SupplierInvoiceLine]
 
